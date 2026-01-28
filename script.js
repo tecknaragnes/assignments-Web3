b2ddde022610884ead4ca6abb56637991033d5f4
Agnes Eriksson <agnes.eriksson.01@gmail.com>
2026-01-28 17:12:10 +0100

    Init commit

---
+++ script.js
@@ -0,0 +1,13 @@
+assignments = []; //array för uppgifterna
+
+// Den globala navigationen ska vara konsekvent på alla sidor.
+// Du skall använda javascript för att dynamiskt generera navigationen på varje sida utifrån en gemensam datastruktur
+// Koden för detta skall ligga i en separat .js-fil som inkluderas i huvudscriptfilen för landningssidan och varje inlämningsuppgift.
+// Koden skall även inkludera logik för att markera den aktuella sidan i navigationen.
+// Innehållet på landningssidan skall bestå av “kort” med korta beskrivningar av varje inlämningsuppgift, inklusive titel och länk till respektive uppgift. Dessa kort skall genereras dynamiskt med JavaScript utifrån samma datastruktur som används för navigationen.
+
+// Varje objekt i datastrukturen ska innehålla följande information:
+// id: En unik identifierare för uppgiften (t.ex. “assignment1”).
+// title: Titeln på uppgiften (t.ex. “Uppgift 1 - Samlingssida för inlämningsuppgifter”).
+// link: Länken till uppgiftens sida (t.ex. “assignment1/index.html”).
+// description: En kort beskrivning av uppgiften.
