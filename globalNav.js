174a8239736e4a9194fbe1e242faf7c1f1689580
Agnes Eriksson <agnes.eriksson.01@gmail.com>
2026-02-02 11:48:11 +0100

    Init commit of globalNav script

---
+++ globalNav.js
@@ -0,0 +1,6 @@
+//exportera till script
+
+// Den globala navigationen ska vara konsekvent på alla sidor.
+// Du skall använda javascript för att dynamiskt generera navigationen på varje sida utifrån en gemensam datastruktur
+// Koden för detta skall ligga i en separat .js-fil som inkluderas i huvudscriptfilen för landningssidan och varje inlämningsuppgift.
+// Koden skall även inkludera logik för att markera den aktuella sidan i navigationen.
