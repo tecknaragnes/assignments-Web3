62112fad1c65d1f0a19aac1a2bd57768f00e9df2
Agnes Eriksson <agnes.eriksson.01@gmail.com>
2026-02-03 17:01:18 +0100

    Init commit wU2

---
+++ assignment2/products.js
@@ -0,0 +1,13 @@
+// Konstbutik
+// minst 8 podukter
+
+const products = [
+    // {
+    //  "id": "",
+    //  "name": "",
+    //  "desc": "",
+    //  "price": "",
+    //  "image": "img/",
+    //  "category": ["minst 2", ""]
+    // } //en vara per objekt
+];
