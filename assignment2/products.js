e0d9e58e3cf1e843e8a63284fbb4bf9945816625
Agnes Eriksson <agnes.eriksson.01@gmail.com>
2026-02-04 11:46:24 +0100

    Add products

--- assignment2/products.js
+++ assignment2/products.js
@@ -1,13 +1,66 @@
-// Konstbutik
-// minst 8 podukter
-
 const products = [
-    // {
-    //  "id": "",
-    //  "name": "",
-    //  "desc": "",
-    //  "price": "",
-    //  "image": "img/",
-    //  "category": ["minst 2", ""]
-    // } //en vara per objekt
+    {
+        "id": "bl01",
+        "name": "Blyertspenna HB",
+        "desc": "En blyerstpenna med mediumhårdhet.",
+        "price": "29",
+        "image": "img/",
+        "category": ["pen", "sketch"]
+    },
+    {
+        "id": "bp01",
+        "name": "Penselpenna 001",
+        "desc": "En filtspetspenna i färgen skogsgrön.",
+        "price": "49",
+        "image": "img/",
+        "category": ["pen", "color", "brush"]
+    },
+    {
+        "id": "br03",
+        "name": "Pensel 3",
+        "desc": "En pensel med mårdhår i storlek 3. Perfekt för akvarell.",
+        "price": "59",
+        "image": "img/",
+        "category": ["color", "brush", "watercolor"]
+    },
+    {
+        "id": "br08",
+        "name": "Pensel 8",
+        "desc": "En pensel med mårdhår i storlek 3. Perfekt för akvarell.",
+        "price": "59",
+        "image": "img/",
+        "category": ["color", "brush", "watercolor"]
+    },
+    {
+        "id": "sp51",
+        "name": "Skissblock A5",
+        "desc": "Ett skissblock i storlek A5. 160 g/m2. 60 ark.",
+        "price": "119",
+        "image": "img/",
+        "category": ["sketch", "pad", "paper"]
+    },
+    {
+        "id": "wp51",
+        "name": "Akvarellblock A5",
+        "desc": "Ett akvarellblock i storlek A5. 300 g/m2. 20 ark. Slät gräng.",
+        "price": "129",
+        "image": "img/",
+        "category": ["watercolor", "pad", "paper"]
+    },
+    {
+        "id": "wc01",
+        "name": "Akvarellfärg 001",
+        "desc": "Akvarellfärg i kopp med färgen magenta.",
+        "price": "29",
+        "image": "img/",
+        "category": ["watercolor", "color"]
+    },
+    {
+        "id": "wc15",
+        "name": "Akvarellfärg 015",
+        "desc": "Akvarellfärg i kopp med färgen koboltblå.",
+        "price": "29",
+        "image": "img/",
+        "category": ["watercolor", "color"]
+    }
 ];
