7beff88a28e06f29a1ac59bf8245d891ce169e08
Agnes Eriksson <agnes.eriksson.01@gmail.com>
2026-02-04 11:17:26 +0100

    Finish assignment cards

--- script.js
+++ script.js
@@ -1,46 +1,18 @@
-let assignments = []; //array för uppgifterna
+const makeCards = (assignments) => {
+    const main = document.getElementById("main");
+    for (let assignment of assignments) {
+        const div = document.createElement("div");
+        const h2 = document.createElement("h2");
+        const para = document.createElement("p");
+        h2.innerHTML = `<a href="${assignment.link}">${assignment.title}</a>`;
+        para.innerHTML = `${assignment.description}`;
+        main.append(div);
+        div.append(h2);
+        div.append(para);
+    }
+}
 
-const wU1 = {
-    id: "wU1",
-    title: "Uppgift 1 - Samlingssida för inlämningsuppgifter",
-    link: "assignment1/index.html",
-    description: "Skapa en landningssida för de framtida uppgifterna i kursen."
-};
-assignments.push(wU1);
-// console.log(assignments);
-
-const wU2 = {
-    id: "wU2",
-    title: "Uppgift 2 - Produktsida med kundvagn",
-    link: "assignment2/index.html",
-    description: "Dynamiskt skapa en produktsida som renderar produkter från objekt, och implementera en kundvagnsfunktion."
-};
-assignments.push(wU2);
-// console.log(assignments);
-
-// const globalNav = document.getElementById("global-nav");
-// globalNav.innerHTML = `
-// <li class="currentPage"><a href="index.html">Start</a></li>
-// <li><a href="${wU1.link}">Uppgift 1</a></li>
-// <li><a href="${wU2.link}">Uppgift 2</a></li>
-// `;
-
-const main = document.getElementById("main");
-// for (let assignment of assignments) {
-main.innerHTML = `
-    <div>
-    <h2><a href="${wU1.link}">${wU1.title}</a></h2>
-    <p>${wU1.description}</p>
-    </div>
-    `;
-main.innerHTML += `
-    <div>
-    <h2><a href="${wU2.link}">${wU2.title}</a></h2>
-    <p>${wU2.description}</p>
-    </div>
-    `;
-// };
-//går det att loopa detta på något sätt? antar genom arrayen, men hur ska jag ta mig igenom arrayen till objekten och egenskaperna däri?
-
-
-// Innehållet på landningssidan skall bestå av “kort” med korta beskrivningar av varje inlämningsuppgift, inklusive titel och länk till respektive uppgift. Dessa kort skall genereras dynamiskt med JavaScript utifrån samma datastruktur som används för navigationen.
+const resp = await fetch("assignments.json"); //hämta objekten för uppgifterna
+// console.log(resp);
+const assignments = await resp.json();
+makeCards(assignments);
