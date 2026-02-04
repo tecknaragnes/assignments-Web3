4cfd7db6de1672323ca8357da22fe0b71a4c47af
Agnes Eriksson <agnes.eriksson.01@gmail.com>
2026-02-04 14:34:14 +0100

    Add product cards in assignment 2

--- assignment2/products.js
+++ assignment2/products.js
@@ -1,6 +1,6 @@
 const products = [
     {
-        "id": "bl01",
+        "id": "bl001",
         "name": "Blyertspenna HB",
         "desc": "En blyerstpenna med mediumhårdhet.",
         "price": "29",
@@ -8,7 +8,7 @@ const products = [
         "category": ["pen", "sketch"]
     },
     {
-        "id": "bp01",
+        "id": "bp001",
         "name": "Penselpenna 001",
         "desc": "En filtspetspenna i färgen skogsgrön.",
         "price": "49",
@@ -16,7 +16,7 @@ const products = [
         "category": ["pen", "color", "brush"]
     },
     {
-        "id": "br03",
+        "id": "br003",
         "name": "Pensel 3",
         "desc": "En pensel med mårdhår i storlek 3. Perfekt för akvarell.",
         "price": "59",
@@ -24,7 +24,7 @@ const products = [
         "category": ["color", "brush", "watercolor"]
     },
     {
-        "id": "br08",
+        "id": "br008",
         "name": "Pensel 8",
         "desc": "En pensel med mårdhår i storlek 3. Perfekt för akvarell.",
         "price": "59",
@@ -32,7 +32,7 @@ const products = [
         "category": ["color", "brush", "watercolor"]
     },
     {
-        "id": "sp51",
+        "id": "sp051",
         "name": "Skissblock A5",
         "desc": "Ett skissblock i storlek A5. 160 g/m2. 60 ark.",
         "price": "119",
@@ -40,15 +40,15 @@ const products = [
         "category": ["sketch", "pad", "paper"]
     },
     {
-        "id": "wp51",
+        "id": "wp051",
         "name": "Akvarellblock A5",
-        "desc": "Ett akvarellblock i storlek A5. 300 g/m2. 20 ark. Slät gräng.",
+        "desc": "Ett akvarellblock i storlek A5. 300 g/m2. 30 ark. Slät gräng.",
         "price": "129",
         "image": "img/",
         "category": ["watercolor", "pad", "paper"]
     },
     {
-        "id": "wc01",
+        "id": "wc001",
         "name": "Akvarellfärg 001",
         "desc": "Akvarellfärg i kopp med färgen magenta.",
         "price": "29",
@@ -56,7 +56,7 @@ const products = [
         "category": ["watercolor", "color"]
     },
     {
-        "id": "wc15",
+        "id": "wc015",
         "name": "Akvarellfärg 015",
         "desc": "Akvarellfärg i kopp med färgen koboltblå.",
         "price": "29",
@@ -64,3 +64,32 @@ const products = [
         "category": ["watercolor", "color"]
     }
 ];
+
+const main = document.getElementById("cardContainers");
+const createCards = (products) => {
+    for (let product of products) {
+        const card = document.createElement("div");
+        const img = document.createElement("div");
+        const name = document.createElement("h2");
+        const desc = document.createElement("p");
+        //kategorier i span?
+        const price = document.createElement("p");
+        const button = document.createElement("button");
+
+        img.classList.add("imgPlaceholder");
+        name.textContent = product.name;
+        desc.textContent = product.desc;
+        price.textContent = `${product.price} kr`;
+        price.classList.add("priceClass");
+        button.textContent = "Lägg till i kundvagnen";
+
+        main.append(card);
+        card.append(img);
+        card.append(name);
+        card.append(desc);
+        // card.append(); //kategorier
+        card.append(price);
+        card.append(button);
+    }
+};
+createCards(products);
