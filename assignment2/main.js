dda9b3bbe1be840a8a8fd722613c3fc27ddbc0ff
Agnes Eriksson <agnes.eriksson.01@gmail.com>
2026-02-10 16:58:58 +0100

    Add console.log functions and more

--- assignment2/main.js
+++ assignment2/main.js
@@ -1,10 +1,17 @@
 import { products } from "./products.js";
 
+if (!localStorage.getItem("productData")) { //se om det finns sparad data i localStorage (är det tomt?)
+    localStorage.setItem("productData", JSON.stringify(products)); //om det är tomt hämtar den data från products och lägger in den i localStorage
+}
+
+
+//----------Skapa produktkort----------
 const main = document.getElementById("cardContainers");
-const createCards = (products) => { //skapa produktkort
+const createCards = (products) => { // funktionen skapa produktkort
     for (let product of products) { //alla produkter
         const card = document.createElement("div"); //kortet
         card.classList.add("productCard");
+        card.classList.add(`${product.id}`)
         main.append(card);
 
         const img = document.createElement("div"); //bildelementet
@@ -37,6 +44,7 @@ const createCards = (products) => { //skapa produktkort
         prNbtn.append(price);
 
         const button = document.createElement("button"); //köp-knapp
+        button.classList.add("addBtn");
         button.textContent = "Lägg till i kundvagnen";
         prNbtn.append(button);
         card.append(prNbtn);
@@ -44,4 +52,38 @@ const createCards = (products) => { //skapa produktkort
 };
 createCards(products); //kör funktionen
 
-import { addToCart } from "./shoppingCart.js";
+
+//----------Rendera kundvagnen----------
+const renderShoppingcart = () => {
+    const cartList = document.getElementById("cartList"); //hitta aside/kundvagn-område
+
+    const data = JSON.parse(localStorage.getItem("productData")); //hämta alla objekt från localStorage
+
+    cartList.innerHTML = "";
+}
+renderShoppingcart();
+
+
+//----------Lägg till i kundvagn----------
+const addToCart = () => {
+    console.log("Lägg till i kundvagnen");
+    //for-of och använd ++${this.count}?
+}
+
+const addButtons = document.querySelectorAll(".addBtn");
+for (let addBtn of addButtons) { //gå igenom alla knappar
+    addBtn.addEventListener("click", addToCart); //lyssna på klick
+}
+
+
+//----------Töm kundvagnen----------
+const emptyCart = () => {
+    cartList.innerHTML = ""; //rensa kundvagnens html
+    console.log("Kundvagn tömd");
+}
+
+const emptyCartBtn = document.getElementById("emptyCart"); //knapp töm kundvagn
+emptyCartBtn.addEventListener("click", emptyCart);
+
+
+// import { addToCart } from "./shoppingCart.js";
