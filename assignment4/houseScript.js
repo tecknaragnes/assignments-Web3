2a8aeaffac858146ecbf7d3292f0b77b3185adbe
Agnes Eriksson <agnes.eriksson.01@gmail.com>
2026-03-13 11:54:18 +0100

    init commit houseScript

---
+++ assignment4/houseScript.js
@@ -0,0 +1,7 @@
+// Detaljsidan (house.html)
+// På detaljsidan visas fullständig information om det valda huset, och här finns också ett bokningsformulär. Här ska också integreras data från ett externt API (antingen väder, platsfoto, karta eller om du själv har en annan idé) som är relevant för husets plats.
+// Hus-id skall läsas av från URL:en.
+// Använd hus-id tillsammans med Array.find() för att hitta rätt hus i datan.
+// Om hus-id saknas eller är ogiltigt (dvs. inget hus med det id:t finns) ska ett tydligt felmeddelande visas i gränssnittet, tillsammans med en länk tillbaka till översiktssidan.
+// Visa fullständig hus-information: bild, namn, plats, beskrivning, pris, skräcknivå (text), spöktyper, WiFi-status.
+// En tydlig länk tillbaka till översiktssidan.
