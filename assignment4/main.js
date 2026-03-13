bc672f6b3da63fde82cd7f32ca04eb2e6211d10a
Agnes Eriksson <agnes.eriksson.01@gmail.com>
2026-03-13 14:36:30 +0100

    Add filters (not complete)

--- assignment4/main.js
+++ assignment4/main.js
@@ -1,3 +1,7 @@
+const errorMsgSpan = document.getElementById("erroeMessage");
+
+
+//----------------------------------------------------------
 let houses;
 try { // Använd try/catch i fetch
     const response = await fetch("houses.json");
@@ -32,19 +36,36 @@ const renderHouses = (house) => {
             </span>`;
         houseDiv.append(houseCard);
     } //scareLevel och hasWifi ska ligga bredvid varandra, och ska göras om till text respektive symbol. Fixa ett ungefärligt samma ratio för bilderna
-
 }
 renderHouses(houses); // Alla hus ska visas när sidan laddas.
 
 
 
 //------------------------------------------------------------
+// let filterResult = houses; //just nu går det att filtrera åt ena hållet, men inte andra
+const filterBase = houses;
+let priceFilter;
+let scareFilter;
+let ghostFilter;
+let wifiFilter;
+
+
+const filterResult = () => {
+    //samla de olika filtrerade listorna och kalla på filtret vid händelselyssnare
+    //borde göra en variabel att stoppa in i renderHuoses-funktionen 
+    // renderHouses();
+    // om resultatet är tomt, visa ett felmeddelande
+}
+
+
 const priceSlider = document.getElementById("maxPrice");
 const priceValue = document.getElementById("priceValue");
 
 priceSlider.addEventListener("input", () => {
-    priceValue.textContent = priceSlider.value;
-    // filtrera med array.filter()
+    const price = Number(priceSlider.value);
+    priceValue.textContent = price;
+    priceFilter = houses.filter(h => h.pricePerNight < price);
+    filterResult();
 });
 
 
@@ -54,8 +75,9 @@ const scareArray = ["Mysigt", "Lite läskigt", "Obehagligt", "Skräckinjagande",
 
 scareSlider.addEventListener("input", () => {
     let index = Number(scareSlider.value) - 1;
-    console.log(index);
     scareValue.textContent = scareArray[index];
+    scareFilter = houses.filter(h => h.scareLevel > index);
+    filterResult();
     // händelselyssnaren funkar, men den fukkar ju lite med texten som ändrar var slidern är, så den skiftar fram och tillbaka när man drar
 });
 
@@ -63,7 +85,10 @@ scareSlider.addEventListener("input", () => {
 const ghostType = document.getElementById("ghostType");
 
 ghostType.addEventListener("change", () => {
-    //array.filter()
+    let ghost = ghostType.value;
+    ghostFilter = filterResult.filter(h => h.ghostTypes == ghost); //detta funkar inte, må ha med att ghostTypes är en array. Hur ska jag gå igenom den?
+    // console.log(filterResult);
+    filterResult();
 })
 
 
@@ -71,14 +96,14 @@ const wifiCheckbox = document.getElementById("wifi");
 
 wifiCheckbox.addEventListener("change", () => {
     if (wifiCheckbox.checked) {
-        //array.filter()
+        wifiFilter = filterResult.filter(h => h.hasWifi == true);
+        filterResult();
     } else {
-        //tömma sökningen
+        filterResult();
     }
 })
-// Filterformulär: typ av spöke (dropdown med alla typer + “Alla typer”)
+
 // När filterformuläret ändras ska husen filtreras i realtid utan att sidan laddas om. Använd input- eller change-event för att fånga ändringar, vilket du tycker passar bäst för varje formulärelement.
-// Filtreringen görs med Array.filter() baserat på de valda kriterierna.
 // Visa meddelande om inga hus matchar filtreringen, det räcker inte att visa en tom sida.
 
 
@@ -92,18 +117,9 @@ wifiCheckbox.addEventListener("change", () => {
 // Översiktssidan (index.html)
 // Varje hus visas med bild, namn, plats, pris per natt, skräcknivå (som text), och en “Läs mer och boka”-knapp som länkar till detaljsidan med rätt id i URL:en (t.ex. house.html?id=1).
 
-// API-integration
-// Extern data via REST API — hämta och visa data från ett externt API med fetch(). Hantera eventuella API-fel med try/catch. Välj minst ett alternativ:
-// A — Väder: Aktuellt väder via Open-Meteo baserat på husets koordinater (gratis, ingen API-nyckel)
-// B — Platsfoto: Foton via Flickr eller liknande baserat på platsnamn eller geo-data (gratis API-nyckel krävs)
-// C — Karta: Interaktiv karta med Leaflet.js + ortsinformation via Nominatim (gratis, ingen API-nyckel)
-// D — Egen idé: Har du en annan idé på relevant data att hämta via ett API? Kör på den! (kolla med läraren först bara)
-// Du bestämmer själv hur och var på detaljsidan du vill visa den externa datan, men den ska vara tydligt kopplad till husets plats. Exempelvis kan väderdata visas i en “Väderprognos”-sektion, platsfoton i ett galleri, och kartan i en “Var ligger huset?”-sektion.
-
-
 
 //to do--------------
-// - try/catch
+// \ try/catch
 // X input/change-eventlyssnare
 // X dynamiskt visa range-value
-// - array.filer() för att visa sökresultat
+// \ array.filer() för att visa sökresultat
