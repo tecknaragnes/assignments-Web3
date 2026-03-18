d45fdb5481344ca06a64a4088f24396c298922e2
Agnes Eriksson <agnes.eriksson.01@gmail.com>
2026-03-18 15:18:18 +0100

    Move renderHouses to new file

---
+++ assignment4/init.js
@@ -0,0 +1,27 @@
+export const scareArray = ["Mysigt", "Lite läskigt", "Obehagligt", "Skräckinjagande", "Ren terror"];
+
+const houseDiv = document.getElementById("houseDiv");
+
+export const renderHouses = (house) => {
+    houseDiv.innerHTML = "";
+
+    for (let h of house) {
+        const houseCard = document.createElement("div");
+        houseCard.classList.add("houseCard");
+        let index = Number(h.scareLevel) - 1;
+
+        houseCard.innerHTML = `
+            <img src="img/${h.image}" alt="">
+            <h3>${h.name}</h3>
+            <p>${h.location}</p>
+            <span class="flexSpan"> 
+                <p class="scares">${scareArray[index]}</p>
+                <p class="wifi">${h.hasWifi}</p>
+            </span>
+            <span class="flexSpan">
+                <p>${h.pricePerNight} kr/natt</p>
+                <a href="house.html?id=${h.id}">Läs mer och boka</a>
+            </span>`;
+        houseDiv.append(houseCard);
+    } // hasWifi ska göras om till symbol eller text
+}
