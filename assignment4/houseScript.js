5304efa5ae1b7a6767dc04b523dea60d8875d81d
Agnes Eriksson <agnes.eriksson.01@gmail.com>
2026-03-28 17:47:15 +0100

    Move renderHouse (details page) to init and add error message

--- assignment4/houseScript.js
+++ assignment4/houseScript.js
@@ -1,4 +1,5 @@
 import { Booking } from "./booking.js";
+import { renderHouse } from "./init.js";
 const errorMsgSpan = document.getElementById("errorMessage");
 errorMsgSpan.style.visibility = "hidden";
 
@@ -17,68 +18,6 @@ catch (err) {
 }
 
 
-//----------------------------------------------------
-const houseInfo = document.getElementById("houseInfoDiv");
-const scareArray = ["Mysigt", "Lite läskigt", "Obehagligt", "Skräckinjagande", "Ren terror"];
-
-const renderHouse = (h) => {
-    houseInfo.innerHTML = "";
-    let index = Number(h.scareLevel) - 1;
-    let wifi;
-    if (h.hasWifi) {
-        wifi = "WiFi finns"
-    } else {
-        wifi = "Inget WiFi"
-    }
-
-    houseInfo.innerHTML = `
-    <span id="textArea">
-        <h2>${h.name}</h2>
-        <p>${h.location}</p>
-        <p>${h.description}</p>
-        <span class="flexObj scaryArea">
-            <p class="scares">${scareArray[index]}</p>
-        </span>
-        <span class="flexObj">
-            <p>${wifi}</p>
-            <p>${h.pricePerNight} kr/natt</p>
-        </span>
-        <div id="map"></div>
-    </span>
-    <img src="img/${h.image}" alt="">`; // Visa fullständig hus-information: bild, namn, plats, beskrivning, pris, skräcknivå (text), spöktyper, WiFi-status.
-
-    const scaryArea = document.querySelector(".scaryArea");
-
-    for (let ghost of h.ghostTypes) { //visa de olika spökena
-        const ghostP = document.createElement("p");
-        ghostP.classList.add("ghost");
-        ghostP.textContent = ghost;
-        scaryArea.append(ghostP);
-    }
-
-
-    //---------------------------------------------------------
-    // Här ska också integreras data från ett externt API
-    let lat = h.coordinates.lat;
-    let long = h.coordinates.lng;
-
-    try {
-        var map = L.map('map').setView([lat, long], 10); L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            maxZoom: 19,
-            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-        }).addTo(map);
-        var marker = L.marker([lat, long]).addTo(map);
-    } catch (err) {
-        map.textContent = "Ett fel har uppstått! 👻 Spöken har fastnat någonstans på webbsidan, så försök igen senare. "
-        map.textContent += `(${err.message})`;
-    }
-
-    // API-integration
-    // Extern data via REST API — hämta och visa data från ett externt API med fetch(). Hantera eventuella API-fel med try/catch. Välj minst ett alternativ:
-    // C — Karta: Interaktiv karta med Leaflet.js + ortsinformation via Nominatim (gratis, ingen API-nyckel)
-
-}
-
 let houseID;
 try { // Använd try/catch
     const urlParams = new URLSearchParams(window.location.search);
@@ -94,6 +33,7 @@ catch (err) { // Om hus-id saknas eller är ogiltigt (dvs. inget hus med det id:
 
 
 //------------------------------------------------------------------
+const bookingDiv = document.getElementById("booking-div");
 const form = document.getElementById("booking-form");
 const dateInput = document.getElementById("start-date");
 const nights = document.getElementById("number-nights");
@@ -103,12 +43,33 @@ const ghostWalk = document.getElementById("ghost-walk");
 const callGhosts = document.getElementById("call-ghosts");
 const code = document.getElementById("code");
 const submitBtn = document.getElementById("submit-booking");
+const errorMsg = document.getElementById("error-msg");
 const booking = new Booking(houseID); // Skapas en instans av Booking när sidan laddas, och koppla den till bokningsformuläret.
 
 form.addEventListener("input", () => {
     //uppdatera info i klassen från formuläret
-    booking.numberOfNights = Number(nights.value);
-    booking.startDate = dateInput.value;
+    try {
+        booking.numberOfNights = Number(nights.value); //try catch här + avaktivera knappen
+        submitBtn.disabled = false;
+        errorMsg.style.visibility = "hidden";
+    } catch (error) {
+        errorMsg.style.visibility = "visible";
+        errorMsg.classList.add("errValid");
+        errorMsg.textContent = error.message;
+        bookingDiv.append(errorMsg);
+        submitBtn.disabled = true;
+    }
+    try {
+        booking.startDate = dateInput.value; //try catch här + avaktivera knappen
+        submitBtn.disabled = false;
+        errorMsg.style.visibility = "hidden";
+    } catch (error) {
+        errorMsg.style.visibility = "visible";
+        errorMsg.classList.add("errValid");
+        errorMsg.textContent = error.message;
+        bookingDiv.append(errorMsg);
+        submitBtn.disabled = true;
+    }
     booking.addOns = {
         dinner: dinner.checked,
         breakfast: breakfast.checked,
@@ -122,6 +83,6 @@ form.addEventListener("input", () => {
 
 submitBtn.addEventListener("click", (e) => {
     e.preventDefault(); //förhindrar sidan från att ladda om
-    console.log("bokad!");
+    console.log("boka klickad");
     booking.validate();
 })
--- assignment4/init.js
+++ assignment4/init.js
@@ -2,7 +2,7 @@ export const scareArray = ["Mysigt", "Lite läskigt", "Obehagligt", "Skräckinja
 
 const houseDiv = document.getElementById("houseDiv");
 
-export const renderHouses = (house) => {
+export const renderHouses = (house) => { // rendera husen på förstasidan
     houseDiv.innerHTML = "";
 
     for (let h of house) {
@@ -29,5 +29,64 @@ export const renderHouses = (house) => {
                 <a href="house.html?id=${h.id}">Läs mer och boka</a>
             </span>`;
         houseDiv.append(houseCard);
+    }
+}
+
+
+
+//----------------------------------------------------
+const houseInfo = document.getElementById("houseInfoDiv");
+// const scareArray = ["Mysigt", "Lite läskigt", "Obehagligt", "Skräckinjagande", "Ren terror"];
+
+export const renderHouse = (h) => { //rendera huset på detaljsidan
+    houseInfo.innerHTML = "";
+    let index = Number(h.scareLevel) - 1;
+    let wifi;
+    if (h.hasWifi) {
+        wifi = "WiFi finns"
+    } else {
+        wifi = "Inget WiFi"
+    }
+
+    houseInfo.innerHTML = `
+    <span id="textArea">
+        <h2>${h.name}</h2>
+        <p>${h.location}</p>
+        <p>${h.description}</p>
+        <span class="flexObj scaryArea">
+            <p class="scares">${scareArray[index]}</p>
+        </span>
+        <span class="flexObj">
+            <p>${wifi}</p>
+            <p>${h.pricePerNight} kr/natt</p>
+        </span>
+        <div id="map"></div>
+    </span>
+    <img src="img/${h.image}" alt="">`; // Visa fullständig hus-information: bild, namn, plats, beskrivning, pris, skräcknivå (text), spöktyper, WiFi-status.
+
+    const scaryArea = document.querySelector(".scaryArea");
+
+    for (let ghost of h.ghostTypes) { //visa de olika spökena
+        const ghostP = document.createElement("p");
+        ghostP.classList.add("ghost");
+        ghostP.textContent = ghost;
+        scaryArea.append(ghostP);
+    }
+
+
+    //---------------------------------------------------------
+    // Här ska också integreras data från ett externt API
+    let lat = h.coordinates.lat;
+    let long = h.coordinates.lng;
+
+    try { // Extern data via REST API — hämta och visa data från ett externt API med fetch(). Hantera eventuella API-fel med try/catch.
+        var map = L.map('map').setView([lat, long], 10); L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 19,
+            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        }).addTo(map);
+        var marker = L.marker([lat, long]).addTo(map);
+    } catch (err) {
+        map.textContent = "Ett fel har uppstått! 👻 Spöken har fastnat någonstans på webbsidan, så försök igen senare. "
+        map.textContent += `(${err.message})`;
     }
 }
